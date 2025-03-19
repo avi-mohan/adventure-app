7ee66c7f59fc72e92b9f8d92e18978cb6c522ab0
Avi <avi.mohan19@gmail.com>
2025-03-18 21:20:36 -0400

    changed activity detail

--- src/components/activities/ActivityDetail.tsx
+++ src/components/activities/ActivityDetail.tsx
@@ -104,26 +104,7 @@ const ActivityDetail = ({ activity }: ActivityDetailProps) => {
                 </div>
               )}
               
-              {/* Website Link if available */}
-              {activity.websiteUrl && (
-                <div className="mb-4">
-                  <div className="border-b border-gray-200 pb-2 mb-4">
-                    <h3 className="font-bold">Provider Website:</h3>
-                  </div>
-                  <a 
-                    href={activity.websiteUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-pink-500 hover:underline flex items-center"
-                  >
-                    Visit Provider Website
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" viewBox="0 0 20 20" fill="currentColor">
-                      <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                      <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-                    </svg>
-                  </a>
-                </div>
-              )}
+              {/* Website Link section has been removed */}
             </div>
           </div>
         </div>
